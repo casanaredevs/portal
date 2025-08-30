387b109fd47c0f8535e686237c604ee8da5b5ede
guillermoplus <luisgal93@hotmail.com>
2025-08-30 14:37:45 -0500

    refactor: segregate home component into smaller components for better maintainability

---
+++ src/frontend/src/app/features/developers/data/developer.data.ts
@@ -0,0 +1,286 @@
+import { Developer, SocialNetworks, Technologies } from "../models/developer.model";
+
+export const developerList: Developer[] = [
+    {
+        name: 'Aldair',
+        lastName: 'Guarupe',
+        imageUrl: 'https://s.gravatar.com/avatar/8d4d674e52413ee001c1619a5dda6271?s=400',
+        description: 'Desarrollador de Apps',
+        socialNetworks: [
+            {
+                name: SocialNetworks.GITHUB,
+                url: '//github.com/aldairguarupe'
+            },
+            {
+                name: SocialNetworks.LINKED_IN,
+                url: '//linkedin.com/in/aldairguarupe/'
+            },
+            {
+                name: SocialNetworks.TWITTER,
+                url: '//twitter.com/aldairguarupe'
+            },
+            {
+                name: SocialNetworks.TELEGRAM,
+                url: '//t.me/aldairguarupe'
+            },
+        ],
+        technologies: [
+            Technologies.ANGULAR,
+            Technologies.IONIC,
+            Technologies.FLUTTER,
+            Technologies.REACT
+        ]
+    },
+    {
+        name: 'Juan David',
+        lastName: 'Pareja Soto.',
+        imageUrl: 'https://avatars.githubusercontent.com/u/3516716',
+        description: 'Desarrollador de Software',
+        socialNetworks: [
+            {
+                name: SocialNetworks.GITHUB,
+                url: '//github.com/parejajd'
+            },
+            {
+                name: SocialNetworks.LINKED_IN,
+                url: '//linkedin.com/in/parejajd/'
+            },
+            {
+                name: SocialNetworks.TWITTER,
+                url: '//twitter.com/parejajd'
+            },
+            {
+                name: SocialNetworks.FACEBOOK,
+                url: '//facebook.com/parejajd'
+            },
+            {
+                name: SocialNetworks.TELEGRAM,
+                url: '//t.me/parejajd'
+            },
+        ]
+    },
+    {
+        name: 'William Ricardo',
+        lastName: 'Torres Curtidor',
+        imageUrl: 'https://avatars.githubusercontent.com/u/8687190',
+        description: 'Fullstack Developer',
+        socialNetworks: [
+            {
+                name: SocialNetworks.GITHUB,
+                url: '//github.com/Rohiri'
+            },
+            {
+                name: SocialNetworks.LINKED_IN,
+                url: '//linkedin.com/in/rohiri/'
+            },
+            {
+                name: SocialNetworks.TWITTER,
+                url: '//twitter.com/rohiriw'
+            },
+            {
+                name: SocialNetworks.FACEBOOK,
+                url: '//facebook.com/rohiriw'
+            },
+            {
+                name: SocialNetworks.TELEGRAM,
+                url: '//t.me/rohiriw'
+            },
+        ]
+    },
+    {
+        name: 'Wilfredo',
+        lastName: 'Torres Ariza',
+        imageUrl: 'https://avatars.githubusercontent.com/u/1329064',
+        description: 'Desarrollador de Software',
+        socialNetworks: [
+            {
+                name: SocialNetworks.GITHUB,
+                url: '//github.com/torreswil'
+            },
+            {
+                name: SocialNetworks.LINKED_IN,
+                url: '//linkedin.com/in/torreswil/'
+            },
+            {
+                name: SocialNetworks.FACEBOOK,
+                url: '//facebook.com/wilfredotorresariza'
+            },
+        ],
+        technologies: [
+            Technologies.LARAVEL,
+            Technologies.VUEJS,
+        ]
+    },
+    {
+        name: 'Rommy Alexander',
+        lastName: 'Duarte Barrra',
+        imageUrl: 'https://avatars.githubusercontent.com/u/4432320',
+        description: '☁️ Arquitecto de Software ☁️ Arquitecto Cloud ☁️ Arquitecto Mobile',
+        socialNetworks: [
+            {
+                name: SocialNetworks.GITHUB,
+                url: '//github.com/romnyd'
+            },
+            {
+                name: SocialNetworks.LINKED_IN,
+                url: '//linkedin.com/in/romnyduarte/'
+            },
+            {
+                name: SocialNetworks.TWITTER,
+                url: '//twitter.com/romnyd'
+            },
+            {
+                name: SocialNetworks.FACEBOOK,
+                url: '//facebook.com/romny.duarte'
+            },
+            {
+                name: SocialNetworks.WHATSAPP,
+                url: '//api.whatsapp.com/send?phone=573114810158'
+            }
+        ],
+        technologies: [
+            Technologies.NET,
+            Technologies.C_SHARP,
+            Technologies.FLUTTER,
+            Technologies.JAVA,
+            Technologies.PHP,
+            Technologies.IONIC,
+            Technologies.NODEJS,
+            Technologies.BLAZOR,
+            Technologies.SPRING
+        ]
+    },
+    {
+        name: 'Luis Guillermo',
+        lastName: 'Galindo Sánchez',
+        imageUrl: 'https://avatars.githubusercontent.com/u/8637358',
+        description: '👨‍💻Arquitecto y 💻Desarrollador de Software | ☁️Arquitecto de Soluciones AWS Asociado Certificado',
+        socialNetworks: [
+            {
+                name: SocialNetworks.GITHUB,
+                url: '//github.com/guillermoplus'
+            },
+            {
+                name: SocialNetworks.LINKED_IN,
+                url: '//linkedin.com/in/guillermoplus/'
+            },
+            {
+                name: SocialNetworks.TWITTER,
+                url: '//twitter.com/guillermoplus'
+            },
+            {
+                name: SocialNetworks.WHATSAPP,
+                url: '//api.whatsapp.com/send?phone=573214325747'
+            }
+        ]
+    },
+    {
+        name: 'Eduardo',
+        lastName: 'Hernández',
+        imageUrl: './../assets/images/dev_jorge.jpg',
+        description: '👨‍💼📝Ingeniero de Sistemas/💻👨‍💻 Desarrollador de Software👨‍🔧',
+        socialNetworks: [
+            {
+                name: SocialNetworks.GITHUB,
+                url: '//github.com/joredher'
+            },
+            {
+                name: SocialNetworks.LINKED_IN,
+                url: '//linkedin.com/in/eduarherz'
+            },
+            {
+                name: SocialNetworks.TWITTER,
+                url: '//twitter.com/EduarHerz'
+            },
+            {
+                name: SocialNetworks.FACEBOOK,
+                url: '//facebook.com/EduarHerz'
+            },
+            {
+                name: SocialNetworks.TELEGRAM,
+                url: '//t.me/EduarHerz'
+            },
+            {
+                name: SocialNetworks.WHATSAPP,
+                url: '//api.whatsapp.com/send?phone=573108731905'
+            }
+        ],
+        technologies: [
+            Technologies.LARAVEL,
+            Technologies.VUEJS,
+            Technologies.PHP,
+            Technologies.JAVASCRIPT,
+            Technologies.A_FRAME
+        ]
+    },
+    {
+        name: 'Camilo',
+        lastName: 'Caquimbo Tabares',
+        imageUrl: 'https://avatars.githubusercontent.com/u/33238233?s=400&u=9d7c45efbf2b62e9fbd486d87a956a46851ce94c&v=4',
+        description: 'Ingeniero de Sistemas - Consultor Salesforce',
+        socialNetworks: [
+            {
+                name: SocialNetworks.GITHUB,
+                url: '//github.com/camilocaquimbo'
+            },
+            {
+                name: SocialNetworks.LINKED_IN,
+                url: '//linkedin.com/in/camilocaquimbo'
+            },
+            {
+                name: SocialNetworks.TWITTER,
+                url: '//twitter.com/ccaquimbo'
+            },
+            {
+                name: SocialNetworks.FACEBOOK,
+                url: '//facebook.com/camilocaquimbotabares'
+            }
+        ]
+    },
+    {
+        name: 'Jonathan',
+        lastName: 'Corredor',
+        imageUrl: 'https://avatars.githubusercontent.com/u/534642?s=400',
+        description: 'Backend Developer',
+        socialNetworks: [
+            {
+                name: SocialNetworks.GITHUB,
+                url: '//github.com/jonathancod'
+            },
+            {
+                name: SocialNetworks.LINKED_IN,
+                url: '//linkedin.com/in/jonathancod'
+            },
+            {
+                name: SocialNetworks.TWITTER,
+                url: '//twitter.com/jonathanc0d'
+            },
+        ],
+        technologies: [
+            Technologies.NODEJS,
+            Technologies.DJANGO,
+            Technologies.C_SHARP,
+        ]
+    },
+    {
+        name: 'Julián',
+        lastName: 'Corredor',
+        imageUrl: 'https://avatars.githubusercontent.com/u/28745532?s=400',
+        description: 'Desarrollador de Software',
+        socialNetworks: [
+            {
+                name: SocialNetworks.GITHUB,
+                url: '//github.com/unmanbarbao'
+            },
+            {
+                name: SocialNetworks.TWITTER,
+                url: '//twitter.com/unmanbarbao'
+            },
+        ],
+        technologies: [
+            Technologies.NODEJS,
+            Technologies.PYTHON,
+            Technologies.VUEJS,
+        ]
+    }
+];
